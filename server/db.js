c1a39ebe474249f7648927a83f2470b72e57ed5a
Siddharth Kumar <165365583+CaptPriClaw@users.noreply.github.com>
2025-06-23 01:18:31 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,17 +1,17 @@
-
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log('MongoDB Connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err.message);
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
     process.exit(1);
   }
 };
 
-export default connectDB;
+module.exports = connectDB;
